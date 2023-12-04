17dacf0c394712f1b69d02ca0acafedbd5cb20b5
Matthew Zimmerman <matt.zimmerman@ccri.com>
2023-12-04 16:35:46 -0500

    part 2 of day 3

--- day3/day3.py
+++ day3/day3.py
@@ -1,10 +1,18 @@
 import argparse
+from dataclasses import dataclass
+from functools import reduce
 import logging
 import re
 import sys
 
 sys.path.append("..")
 import aoc
+
+@dataclass
+class Number:
+    i: int
+    j: int
+    numbertext: str
 
 
 class Grid:
@@ -21,9 +29,10 @@ class Grid:
             self.height = len(self.data)
             if self.height > 0:
                 self.width = len(self.data[0])
+        self.numbers = self.find_numbers()
 
     def get(self, i, j):
-        if i >= 0 and i < self.width and j >= 0 and j < self.height:
+        if 0 <= i < self.width and 0 <= j < self.height:
             return self.data[j][i]
         else:
             return '.'
@@ -36,9 +45,9 @@ class Grid:
                 c = self.get(i, j)
                 if Grid.digitpatt.match(c):
                     if in_number:
-                        numbers[-1]['number'] += c
+                        numbers[-1].numbertext += c
                     else:
-                        numbers.append({"pos": (i, j), "number": c})
+                        numbers.append(Number(i=i, j=j, numbertext=c))
                         in_number = True
                 else:
                     if in_number:
@@ -47,24 +56,44 @@ class Grid:
         return numbers
 
     def is_part(self, number):
-        ni = number["pos"][0]
-        nj = number["pos"][1]
-        for j in range(nj-1, nj+2):
-            for i in range(ni-1, ni+len(number["number"])+1):
+        for j in range(number.j-1, number.j+2):
+            for i in range(number.i-1, number.i+len(number.numbertext)+1):
                 char = self.get(i, j)
-                logging.debug(f"pos={i, j}, char={self.get(i, j)}")
+                #print(f"{number} char={self.get(i, j)}")
                 if char != "." and not Grid.digitpatt.match(char):
                     return True
         return False
 
     def part1(self):
         total = 0
-        for number in self.find_numbers():
+        for number in self.numbers:
             number_is_part = self.is_part(number)
-            logging.info(f"{number['number']} {number_is_part}")
+            print(f"{number} {number_is_part}")
             if number_is_part:
-                total += int(number['number'])
+                total += int(number.numbertext)
         return total
+
+    def part2(self):
+        totalratio = 0
+        numbers = self.find_numbers()
+        for j in range(self.height):
+            for i in range(self.width):
+                c = self.get(i, j)
+                if c == '*':
+                    gears = self.find_gears(i, j, numbers)
+                    print(gears)
+                    if len(gears) == 2:
+                        ratio = reduce(lambda a, b: a * b, gears)
+                        #print(gears, ratio)
+                        totalratio += ratio
+        return totalratio
+
+    def find_gears(self, i, j, numbers):
+        gears = []
+        for num in numbers:
+            if i >= num.i-1 and i <= num.i+len(num.numbertext) and j >= num.j-1 and j <= num.j+1:
+                gears.append(int(num.numbertext))
+        return gears
 
     def print_grid(self):
         for l in self.data:
@@ -75,7 +104,8 @@ def main(args):
     #grid.print_grid()
     #print(grid.width)
     #print(grid.height)
-    print(grid.part1())
+    print(f"part1 = {grid.part1()}")
+    print(f"part2 = {grid.part2()}")
 
 
 if __name__ == '__main__':
