af9ab9f1c7819d1e32e2ecf0375c027cfe0920f0
Matthew Zimmerman <matt.zimmerman@ccri.com>
2023-12-14 10:30:35 -0500

    solve day 6 with some math

--- day6/day6.py
+++ day6/day6.py
@@ -1,7 +1,9 @@
 import argparse
 from functools import reduce
 import logging
+import math
 import sys
+
 
 sys.path.append("..")
 import aoc
@@ -12,7 +14,13 @@ def read_input(filename):
     ds = [int(d) for d in (lines[1].split())[1:]]
     return ts, ds
 
-def max_distance(tmax: int, distance_record: int):
+def read_input2(filename):
+    lines = aoc.read_lines(filename)
+    t = int("".join((lines[0].split())[1:]))
+    d = int("".join((lines[1].split())[1:]))
+    return t, d
+
+def part1_wins(tmax: int, distance_record: int):
     wins = 0
     for t in range(tmax):
         tleft = tmax - t
@@ -22,20 +30,34 @@ def max_distance(tmax: int, distance_record: int):
             wins += 1
     return wins
 
+def part2_wins(T: int, D: int):
+    pred = math.sqrt(T*T - 4 * D)
+    logging.info(pred)
+    wmin = (T - pred)/2
+    wmax = (T + pred)/2
+    logging.info(f"{wmin} {wmax}")
+
+    return math.floor(wmax) - math.ceil(wmin) + 1
+
 def product(xs):
     return reduce(lambda a, b: a * b, xs, 1)
 
 def main(args):
     ts, ds = read_input(args.filename)
 
-
+    print("-- part 1 --")
     logging.info(f"{ts}")
     logging.info(f"{ds}")
     nwins = []
     for t, d in zip(ts, ds):
-        nwins.append(max_distance(t, d))
+        nwins.append(part1_wins(t, d))
     logging.info(f"{nwins}")
-    print(f"part1 = {product(nwins)}")
+    print(f"{product(nwins)}")
+
+    print("-- part 2 --")
+    t, d = read_input2(args.filename)
+    logging.info(f"{t} {d}")
+    print(f"{part2_wins(t, d)}")
 
 
 if __name__ == '__main__':
