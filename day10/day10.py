88e3415ff962c338c3717daa4f5e6d29bdd9a54c
Matthew Zimmerman <matt.zimmerman@ccri.com>
2024-01-15 11:50:49 -0500

    working on day 10 part 2

--- day10/day10.py
+++ day10/day10.py
@@ -1,5 +1,4 @@
 import argparse
-import logging
 import sys
 
 sys.path.append("..")
@@ -14,6 +13,7 @@ class Grid:
 
     BYCHAR = {
         ".": {},
+        " ": {},
         "-": {"E", "W"},
         "|": {"N", "S"},
         "F": {"E", "S"},
@@ -23,15 +23,49 @@ class Grid:
         "S": {"N", "E", "S", "W"}
     }
 
-    def __init__(self, filename):
-        self.grid = []
-        self.height = 0
-        self.width = 0
+    @classmethod
+    def fromfile(cls, filename):
+        grid = list()
         with open(filename, "r") as fh:
             for l in fh:
-                self.grid.append(l.strip())
-            self.width = len(self.grid[0])
-            self.height = len(self.grid)
+                grid.append(l.strip())
+        return cls(grid)
+
+    def __init__(self, grid):
+        if not len(grid):
+            raise ValueError("Input grid is empty")
+        self.grid = list(grid)
+        self.width = len(self.grid[0])
+        self.height = len(self.grid)
+
+    def expand(self):
+        expandx = []
+        for j in range(len(self.grid)):
+            expandx.append("")
+            expandx[j] += self.grid[j][0]
+            for i in range(len(self.grid[j])-1):
+                ca = self.grid[j][i]
+                cb = self.grid[j][i+1]
+                if 'E' in Grid.BYCHAR[ca] and 'W' in Grid.BYCHAR[cb]:
+                    expandx[j] += "-"
+                else:
+                    expandx[j] += " "
+                expandx[j] += cb
+
+        expandxy = []
+        expandxy.append(expandx[0])
+        for j in range(len(expandx)-1):
+            fill = ""
+            for i in range(len(expandx[j])):
+                ca = expandx[j][i]
+                cb = expandx[j+1][i]
+                if 'S' in Grid.BYCHAR[ca] and 'N' in Grid.BYCHAR[cb]:
+                    fill += "|"
+                else:
+                    fill += " "
+            expandxy.append(fill)
+            expandxy.append(expandx[j+1])
+        return Grid(expandxy)
 
     def prettyprint(self):
         for l in self.grid:
@@ -78,7 +112,7 @@ class Grid:
 
         while queue:  # Creating loop to visit each node
             p, steps = queue.pop(0)
-            print(p, steps)
+            #print(p, steps)
             if steps > maxsteps:
                 maxsteps = steps
 
@@ -87,10 +121,10 @@ class Grid:
                     visited.add(np)
                     queue.append((np, steps+1))
 
-        return maxsteps
+        return maxsteps, visited
 
 def main(args):
-    grid = Grid(args.filename)
+    grid = Grid.fromfile(args.filename)
     grid.prettyprint()
 
     print()
@@ -101,21 +135,15 @@ def main(args):
     p11 = Point(1, 1)
     print(p11, grid.next(p11))
 
-    grid.bfs()
+    #grid.bfs()
+    gridexp = grid.expand()
+    gridexp.prettyprint()
 
 if __name__ == '__main__':
     # parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('--filename', '-f', default="input.txt")
     parser.add_argument('--log', '-l', choices=["debug", "info", "warning"])
-    args = parser.parse_args("-f test2.txt".split())
-
-    # parse logging level
-    loglevel = logging.INFO
-    if args.log == "debug":
-        loglevel = logging.DEBUG
-    elif args.log == "warning":
-        loglevel = logging.WARNING
-    logging.basicConfig(level=loglevel, stream=sys.stdout)
+    args = parser.parse_args("-f test5.txt".split())
 
     main(args)
