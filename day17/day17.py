344eea123d4367bc25c3cfe705a1c29e71f35c3a
Matthew Zimmerman <matt.zimmerman@ccri.com>
2024-11-14 12:50:01 -0500

    fixed problems with part #2 of day 17

--- day17/day17.py
+++ day17/day17.py
@@ -21,42 +21,20 @@ class Grid:
         self.data = data
         self.width = data.shape[0]
         self.height = data.shape[1]
-
-    def nextdir(self, state: State, crucible: str='normal'):
-        if crucible == 'normal':
-            return self.nextdir_normal(state)
-        elif crucible == 'ultra':
-            return self.nextdir_ultra(state)
-        else:
-            raise Exception('bad crucible type')
     
-    def nextdir_normal(self, state: State):
-        out = []
-        if state.dir == 'E' or state.dir == 'W':
-            out.append('N')
-            out.append('S')
-        elif state.dir == 'N' or state.dir == 'S':
-            out.append('E')
-            out.append('W')
-        if state.n < 3:
-            out.append(state.dir)
-        return out
-
-    def nextdir_ultra(self, state: State):
-        out = []
-        if state.n < 10:
-            out.append(state.dir)
-        if state.n >= 4:
+    def neighbors(self, state: State, minsteps=0, maxsteps=3):
+        nextdirs = []
+        if state.n < maxsteps:
+            nextdirs.append(state.dir)
+        if state.n >= minsteps:
             if state.dir == 'E' or state.dir == 'W':
-                out.append('N')
-                out.append('S')
+                nextdirs.append('N')
+                nextdirs.append('S')
             elif state.dir == 'N' or state.dir == 'S':
-                out.append('E')
-                out.append('W')
-        return out
-    
-    def neighbors(self, state: State, crucible='normal'):
-        for ndir in self.nextdir(state, crucible):
+                nextdirs.append('E')
+                nextdirs.append('W')
+        
+        for ndir in nextdirs:
             npoint = state.point.move(ndir)
             if npoint.x < 0 or npoint.y < 0 or npoint.x >= self.width or npoint.y >= self.height:
                 continue
@@ -68,30 +46,29 @@ class Grid:
                 nn = 1
             yield State(nloss, npoint, ndir, nn)
 
-    def dijkstra(self, start=None, end=None, crucible='normal'):
+    def dijkstra(self, start=None, end=None, minsteps=0, maxsteps=3):
         if start == None:
             start = Point(0, 0)
         if end == None:
             end = Point(self.width-1, self.height-1)
-        startstate = State(0, start, 'E', 0)
         
         visited = set()
-
         pq = queue.PriorityQueue()
-        pq.put(startstate)
+        pq.put(State(0, start, 'E', 0))
+        pq.put(State(0, start, 'S', 0))
 
         while not pq.empty():
             state = pq.get()
             #print(state)
 
-            if state.point == end:
+            if state.point == end and state.n >= minsteps:
                 return state
 
             if (state.point, state.dir, state.n) in visited:
                 continue
             visited.add((state.point, state.dir, state.n))
             
-            for nstate in self.neighbors(state, crucible):
+            for nstate in self.neighbors(state, minsteps=minsteps, maxsteps=maxsteps):
                 pq.put(nstate)
 
     @classmethod
@@ -105,14 +82,15 @@ class Grid:
 
 def main(args):
     grid = Grid.fromfile(args.filename)
-    print(grid.data)
-    print(grid.width)
-    print(grid.height)
+    #print(grid.data)
+    #print(grid.width)
+    #print(grid.height)
 
-    s1 = State(0, Point(0, 0), 'E', 0)
-    print(list(grid.neighbors(s1)))
+    #s1 = State(0, Point(0, 0), 'E', 0)
+    #print(list(grid.neighbors(s1)))
+    
     print(grid.dijkstra())
-    print(grid.dijkstra(crucible='ultra'))
+    print(grid.dijkstra(minsteps=4, maxsteps=10))
 
 if __name__ == '__main__':
     # parse arguments
